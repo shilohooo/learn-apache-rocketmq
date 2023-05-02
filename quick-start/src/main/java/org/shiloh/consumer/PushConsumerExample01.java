18d2ed51334698aae9861945e0832ec4d9a3e7ce
shiloh <lixiaolei595@gmail.com>
2023-05-02 23:30:52 +0800

    add quick-start module

---
+++ quick-start/src/main/java/org/shiloh/consumer/PushConsumerExample01.java
@@ -0,0 +1,57 @@
+package org.shiloh.consumer;
+
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.consumer.ConsumeResult;
+import org.apache.rocketmq.client.apis.consumer.FilterExpression;
+import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
+import org.apache.rocketmq.client.apis.consumer.PushConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
+/**
+ * RocketMQ消费者代码示例
+ * <p>
+ * 创建订阅普通消息的消费者
+ *
+ * @author shiloh
+ * @date 2023/5/2 23:11
+ */
+public class PushConsumerExample01 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PushConsumerExample01.class);
+
+    public static void main(String[] args) throws Exception {
+        // 接入点地址：需要设置成Proxy的地址和端口列表，一般是：xxx:8081;xxx:8081
+        final String endpoint = "172.29.67.83:8081";
+        final ClientConfiguration configuration = ClientConfiguration.newBuilder()
+                .setEndpoints(endpoint)
+                .build();
+        // 订阅消息的过滤规则，“*”表示订阅所有Tag的消息
+        final String tag = "*";
+        final FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
+        // 为消费者指定所属的消费者分组，Group 需要提前创建
+        final String consumerGroup = "TestGroup";
+        // 指定需要订阅哪个目标Topic，Topic需要提前创建
+        final String topic = "TestTopic";
+        // 初始化PushConsumer，需要绑定消费者分组、通信参数以及订阅关系
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        final PushConsumer pushConsumer = provider.newPushConsumerBuilder()
+                .setClientConfiguration(configuration)
+                // 设置消费者分组
+                .setConsumerGroup(consumerGroup)
+                // 设置预绑定的订阅关系
+                .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
+                // 设置消费监听器
+                .setMessageListener(messageView -> {
+                    // 处理消息并返回消费结果
+                    LOGGER.info("Consume message successfully, messageId = {}", messageView.getMessageId());
+                    return ConsumeResult.SUCCESS;
+                })
+                .build();
+        Thread.sleep(Long.MAX_VALUE);
+        // 如果不需要再使用 PushConsumer，可以关闭该实例
+        pushConsumer.close();
+    }
+}
---
+++ quick-start/src/main/java/org/shiloh/producer/ProducerExample01.java
@@ -0,0 +1,57 @@
+package org.shiloh.producer;
+
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * RocketMQ生产者示例代码
+ * <p>
+ * 发送普通消息
+ *
+ * @author shiloh
+ * @date 2023/5/2 22:17
+ */
+public class ProducerExample01 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerExample01.class);
+
+    public static void main(String[] args) throws ClientException {
+        // 接入点地址：需要设置成Proxy的地址和端口列表，一般是：xxx:8081;xxx:8081
+        final String endpoint = "172.29.67.83:8081";
+        // 消息发送的目标Topic名称，需要提前创建
+        final String topic = "TestTopic";
+
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        final ClientConfiguration configuration = ClientConfiguration.newBuilder()
+                .setEndpoints(endpoint)
+                .build();
+        // 初始化生产者时需要设置通信配置以及预绑定的Topic
+        try (final Producer producer = provider.newProducerBuilder()
+                .setTopics(topic)
+                .setClientConfiguration(configuration)
+                .build()) {
+            // 普通消息发送
+            final Message message = provider.newMessageBuilder()
+                    .setTopic(topic)
+                    // 设置消息索引键，可根据关键字精确查找某条消息
+                    .setKeys("MessageKey")
+                    // 设置消息 Tag，用于消费端根据指定 Tag 过滤消息
+                    .setTag("MessageTag")
+                    // 消息体
+                    .setBody("MessageBody".getBytes(StandardCharsets.UTF_8))
+                    .build();
+            final SendReceipt sendReceipt = producer.send(message);
+            LOGGER.info("Send message successfully, messageId = {}", sendReceipt.getMessageId());
+        } catch (IOException e) {
+            LOGGER.error("Failed to send message", e);
+        }
+    }
+}
