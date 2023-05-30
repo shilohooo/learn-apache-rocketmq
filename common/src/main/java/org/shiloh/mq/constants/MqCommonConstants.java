5de4e175a667c11f90dc9c514e297919aeb1ab46
shiloh <lixiaolei595@gmail.com>
2023-05-30 21:42:48 +0800

    refactor: add common module

---
+++ common/src/main/java/org/shiloh/mq/constants/MqCommonConstants.java
@@ -0,0 +1,16 @@
+package org.shiloh.mq.constants;
+
+/**
+ * RocketMQ 通用常量
+ *
+ * @author shiloh
+ * @date 2023/5/30 21:24
+ */
+public final class MqCommonConstants {
+    private MqCommonConstants() {}
+
+    /*
+     * 服务端地址
+     */
+    public static final String ENDPOINT = "172.29.67.83:8081";
+}
