054828316f38f1ac6a0c3253a16847da003a9d85
shiloh <lixiaolei595@gmail.com>
2023-06-03 17:55:54 +0800

    chore: optimize imports

--- spring-boot-integration/src/test/java/org/shiloh/test/RocketMqProducerTests.java
+++ spring-boot-integration/src/test/java/org/shiloh/test/RocketMqProducerTests.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 
