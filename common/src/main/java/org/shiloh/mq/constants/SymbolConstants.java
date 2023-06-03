b00585d22691498f7e3bd21b93c57a0ea06cf6f1
shiloh <lixiaolei595@gmail.com>
2023-06-03 17:54:58 +0800

    refactor: add SymbolConstants

---
+++ common/src/main/java/org/shiloh/mq/constants/SymbolConstants.java
@@ -0,0 +1,17 @@
+package org.shiloh.mq.constants;
+
+/**
+ * 符号相关常量
+ *
+ * @author shiloh
+ * @date 2023/6/3 17:37
+ */
+public final class SymbolConstants {
+    private SymbolConstants() {
+    }
+
+    /**
+     * 英文冒号
+     */
+    public static final String COLON = ":";
+}
